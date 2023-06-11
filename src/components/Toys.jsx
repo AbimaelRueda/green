37167be7c9f600d5045f2de9c6f8afa7a52c40a4
Abimael Rueda <aries_abimael@hotmail.com>
2023-06-11 00:23:39 -0600

    error

--- src/components/Toys.jsx
+++ src/components/Toys.jsx
@@ -55,7 +55,7 @@ export default function Toys() {
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {toys.map((product) => (
+            {example.map((product) => (
               <a key={product.id} href={product.href} className="group">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7">
                   <img
